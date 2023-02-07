9efd8556e9cbc072504ff0492fe030442758999e
Esteban <estebaanbisocoli@gmail.com>
2023-02-07 20:42:46 -0300

    Add ReactChallange component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.scss';
 import ReactChallange from './components/ReactChallenge/ReactChallange';
 
 function App() {
   return (
-    <div className="App">
+    
       <ReactChallange />
-    </div>
   );
 }
 
