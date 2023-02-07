f44897260b1b55a862732f4051e7c416371dc976
Esteban <estebaanbisocoli@gmail.com>
2023-02-07 20:01:34 -0300

    Add i18n and strings

--- src/components/ReactChallenge/ReactChallange.tsx
+++ src/components/ReactChallenge/ReactChallange.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next';
 export default function ReactChallange() {
-  return (
-    <div>ReactChallange</div>
-  )
+    const { t } = useTranslation()
+    return (
+        <div>{t("NAVBAR_CART")}</div>
+    )
 }
---
+++ src/strings/i18n.ts
@@ -0,0 +1,14 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json";
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  lng: "en-US",
+  resources: {
+    en: {
+      translation: en,
+    },
+  },
+});
+
+export default i18n;
