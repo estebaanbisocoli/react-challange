7c819277aebd54a2627a13743f04e290286ec61d
Esteban <estebaanbisocoli@gmail.com>
2023-02-09 22:56:40 -0300

    add all cart products

--- src/components/CartItems/CartItems.tsx
+++ src/components/CartItems/CartItems.tsx
@@ -1,26 +1,65 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { ICartItem, IOnCartItemRemove, IOnQuantityChangeCallback, IPack, IProduct, ProductType } from '../../model/type'
-import "./CartItems.scss"
-import CartPack from './CartPack'
-import CartProduct from './CartProduct'
-export default function CartItems({ cart, onQuantityChange, onCartItemRemove }: { cart: ICartItem[], onQuantityChange:IOnQuantityChangeCallback, onCartItemRemove: IOnCartItemRemove}) {
-  const { t } = useTranslation()
+import React from "react";
+import { useTranslation } from "react-i18next";
+import {
+  ICartItem,
+  IOnCartItemRemove,
+  IOnQuantityChangeCallback,
+  IPack,
+  IProduct,
+  ProductType,
+} from "../../model/type";
+import "./CartItems.scss";
+import CartPack from "./CartPack";
+import CartProduct from "./CartProduct";
+
+const Divider = () => <div className="cart-items-divider"></div>
+export default function CartItems({
+  cart,
+  onQuantityChange,
+  onCartItemRemove,
+}: {
+  cart: ICartItem[];
+  onQuantityChange: IOnQuantityChangeCallback;
+  onCartItemRemove: IOnCartItemRemove;
+}) {
+  const { t } = useTranslation();
   return (
-    <div className='cart-items-container'>
-      <div className='cart-items-title'>
-        <h2 className='text-lg bold lh24'>{t("CART_TITLE")}</h2>
-        <span className='text-md  regular lh24'>({cart.length})</span>
+    <div className="cart-items-container">
+      <div className="cart-items-title">
+        <h2 className="text-lg bold lh24">{t("CART_TITLE")}</h2>
+        <span className="text-md  regular lh24">({cart.length})</span>
       </div>
-      <div className='cart-items-product-list'>
+      <div className="cart-items-product-list">
         {cart.map((cartItem) => {
           if (cartItem.product.type === ProductType.PACK) {
-            return <CartPack pack={cartItem.product as IPack} quantity={cartItem.quantity} key={cartItem.product.id} onQuantityChange={onQuantityChange} onRemove={onCartItemRemove} />
+            return (
+              <>
+                <CartPack
+                  pack={cartItem.product as IPack}
+                  quantity={cartItem.quantity}
+                  key={cartItem.product.id}
+                  onQuantityChange={onQuantityChange}
+                  onRemove={onCartItemRemove}
+                />
+                <Divider />
+              </>
+            );
           } else if (cartItem.product.type === ProductType.SINGLE) {
-            return <CartProduct product={cartItem.product} quantity={cartItem.quantity} key={cartItem.product.id} />
+            return (
+              <>
+                <CartProduct
+                  product={cartItem.product}
+                  quantity={cartItem.quantity}
+                  key={cartItem.product.id}
+                  onQuantityChange={onQuantityChange}
+                  onRemove={onCartItemRemove}
+                />
+                <Divider />
+              </>
+            );
           }
         })}
       </div>
     </div>
-  )
+  );
 }
--- src/components/CartItems/CartProduct.tsx
+++ src/components/CartItems/CartProduct.tsx
@@ -1,8 +1,65 @@
-import React from 'react'
-import { IProduct } from '../../model/type'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import {
+  IOnCartItemRemove,
+  IOnQuantityChangeCallback,
+  IProduct,
+} from "../../model/type";
+import QuantitySelect from "../QuantitySelect/QuantitySelect";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { formatCurrencyUSD } from "../../utils/currency";
 
-export default function CartProduct({ product, quantity, onEdit, onRemove }: { product: IProduct, quantity: number, onEdit?: () => void, onRemove?: () => void }) {
+export default function CartProduct({
+  product,
+  quantity,
+  onRemove,
+  onQuantityChange,
+}: {
+  product: IProduct;
+  quantity: number;
+  onRemove: IOnCartItemRemove;
+  onQuantityChange: IOnQuantityChangeCallback;
+}) {
+  const { t } = useTranslation();
   return (
-    <div></div>
-  )
+    <div className="cart-item-container">
+      <div className="cart-item-image-container">
+        <img
+          src={product.image}
+          width={120}
+          height={120}
+          alt={product.name}
+        ></img>
+      </div>
+      <div className="cart-item-description">
+        <h2 className="text-md lh24 bold">{product.name}</h2>
+        <QuantitySelect
+          value={quantity}
+          text={t("CART_QUANTITY")}
+          max={51}
+          onChange={(value) => onQuantityChange(product, value)}
+        />
+        <a
+          className="cart-item-product-delete"
+          onClick={() => onRemove(product)}
+        >
+          <DeleteIcon className="cart-item-product-icon" />
+          <span className="text-xxxs lh16 semi-bold">{t("CART_REMOVE")}</span>
+        </a>
+      </div>
+      <div className="cart-item-price">
+        <h2 className="text-md lh16 bold primary-color">
+          {formatCurrencyUSD(product.price as number)}
+        </h2>
+        <div className="cart-item-total-container">
+          <span className="text-md lh16 semi-bold primary-color">
+            {t("CART_TOTAL")}
+          </span>
+          <span className="text-md lh16 bold primary-color">
+            {formatCurrencyUSD((product.price as number) * quantity)}
+          </span>
+        </div>{" "}
+      </div>
+    </div>
+  );
 }
