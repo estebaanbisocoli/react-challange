f55d541af909173104c66d0f71fd24af6b2e3dd1
Esteban <estebaanbisocoli@gmail.com>
2023-02-09 21:25:08 -0300

    Make quantity select propagate changes

--- src/components/QuantitySelect/QuantitySelect.tsx
+++ src/components/QuantitySelect/QuantitySelect.tsx
@@ -9,7 +9,6 @@ export default function QuantitySelect({ className, text, max, value, onChange }
             <h3 className='text-normal lh-16 semi-bold'>{text}</h3>
             <Select value={value} onChange={(evt) => onChange(Number(evt.target.value))} className="quantity-select-select" MenuProps={{ sx: { maxHeight: "400px" } }} IconComponent={KeyboardArrowDownIcon}>
                 {Array(max).fill(0).map((_, index) => <MenuItem value={index} key={index}>{index}</MenuItem>)}
-
             </Select>
 
         </div>
