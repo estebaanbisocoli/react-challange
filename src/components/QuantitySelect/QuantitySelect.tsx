17dd6cee8e36768faf9ee96a7e3a415c5c832f5d
Esteban <estebaanbisocoli@gmail.com>
2023-02-09 21:11:32 -0300

    Add Quantity select component

---
+++ src/components/QuantitySelect/QuantitySelect.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./QuantitySelect.scss"
+import classNames from 'classnames'
+import { MenuItem, Select } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+export default function QuantitySelect({ className, text, max, value, onChange }: { className?: string, text: string, max: number, value: number, onChange: (value: number) => void, }) {
+    return (
+        <div className={classNames("quantity-select-container", className)}>
+            <h3 className='text-normal lh-16 semi-bold'>{text}</h3>
+            <Select value={value} onChange={(evt) => onChange(Number(evt.target.value))} className="quantity-select-select" MenuProps={{ sx: { maxHeight: "400px" } }} IconComponent={KeyboardArrowDownIcon}>
+                {Array(max).fill(0).map((_, index) => <MenuItem value={index} key={index}>{index}</MenuItem>)}
+
+            </Select>
+
+        </div>
+    )
+}
